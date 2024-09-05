05ec7d65fe81290b98326d55f262ae1c456777ab
ElvinMaharramov <elvinmaharramov98@gmail.com>
2024-09-05 13:26:19 +0400

    authOptions added in nextauth.ts file

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,53 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // Diğer sağlayıcılar ekleyebilirsiniz
+  ],
+/*   pages: {
+    signIn: '/auth/signin',  // Giriş sayfanızı belirleyin
+    // Diğer sayfalar varsa buraya ekleyin
+  }, */
+  // Diğer NextAuth yapılandırmaları
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+export default handler;
+
+
+/* import NextAuth from "next-auth";
 
 import { authOptions } from "../../../../lib/authOptions";
 
 const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST};
-export default NextAuth(authOptions);
+export default NextAuth(authOptions); */
 
+/* export default NextAuth({
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+      // Diğer sağlayıcılar
+    ],
+    // Diğer yapılandırma
+    pages: {
+      signIn: '/auth/signin',  // veya gerekli diğer sayfalar
+    },
+  }); */
 
 // import GithubProvider from 'next-auth/providers/github';
 // import GoogleProvider from 'next-auth/providers/google';
@@ -47,3 +89,5 @@ export default NextAuth(authOptions);
 //     }),
 //   ],
 // });
+// [...nextauth].js veya [...nextauth].ts dosyasındaki yapılandırma
+  
